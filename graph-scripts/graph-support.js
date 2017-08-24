2cfc113e3b7f4d86dae972bec13626ed3978b3f8
Warren Drucker <warren.drucker@garmin.com>
2017-08-24 18:19:24 -0500

    Loads Ajax stuff
    Change-Id: Ic176e6b1c500155fd8ce4ab4cb18377968cda6e6

--- graph-scripts/graph-support.js
+++ graph-scripts/graph-support.js
@@ -2,3 +2,25 @@ function changeData(){
   var chart = document.getElementById("pieChart");
   chart.setData([[50],[50]],0);
 }
+
+function loadData(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      retrieveData(this);
+    }
+  };
+  xhttp.open("GET","power_data.xml",true);
+  xhttp.send();
+}
+function retrieveData(xml){
+  var i;
+  var xmlDoc = xml.responseXML;
+  var xmlData = xmlDoc.getElementsByTagName("SOURCE");
+  var newChartData = [];
+  var chart = document.getElementById("pieChart");
+  for(i=0;i<xmlData.length;i++){
+    newChartData[i] = xmlData[i].getElementsByTagName("CURRENT")[0].childNodes[0].nodeValue
+  }
+  chart.setData(newChartData,0);
+}
