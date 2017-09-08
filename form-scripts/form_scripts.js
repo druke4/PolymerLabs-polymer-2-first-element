233e22ec5c8dc9c2e71fe98013d7c373ea37e92d
Warren Drucker <warren.drucker@garmin.com>
2017-09-08 17:28:22 -0500

    Start of form things
    Change-Id: Ieccc6958ce40aede369b74c1a5b5db4180d7efa9

---
+++ form-scripts/form_scripts.js
@@ -0,0 +1,43 @@
+function addFields(){
+    // Number of inputs to create
+    var number = document.getElementById("sources").value;
+    // Container <div> where dynamic content will be placed
+    var container = document.getElementById("sourceContainer");
+    // Clear previous contents of the container
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+    for (i=0;i<number;i++){
+        // Append a node with a random text
+        container.appendChild(document.createTextNode("Source " + (i+1)));
+        // Create an <input> element, set its type and name attributes
+        var input = document.createElement("input");
+        input.type = "text";
+        input.name = "member" + i;
+        container.appendChild(input);
+        // Append a line break
+        container.appendChild(document.createElement("br"));
+    }
+  }
+
+function validateFormOnSubmit(theForm) {
+    var reason = "";
+    reason += validateSourceMemebers(theForm.name);
+
+    if (reason != "") {
+        alert("Some fields need correction:\n" + reason);
+    } else {
+        var mySettingsObj = {
+          "DataRate":document.getElementsByName('DataRate').value;,
+          "Range":document.getElementsByName('Range').value;,
+          "Resolution":document.getElementsByName('Resolution').value;,
+          "Sources":document.getElementsByName('Sources').value;,
+          for(i=0;i<document.getElementsByName('Sources').value;i++){
+            "Source"+ i : document.getElementsByName("member" + i).value;,
+          }
+        }
+        var mySettingsObjString = JSON.stringify(mySettingsObj);
+        simpleCart.checkout();
+    }
+    return false;
+}
