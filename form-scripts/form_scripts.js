d66656cc5c0d6d5e4e25f4463c4ffccc3018e624
Warren Drucker <warren.drucker@garmin.com>
2017-09-12 10:38:24 -0500

    Fixed source changing javascript
    Change-Id: Ic1eceb19d0faacf86992bec811d2fbba4bc66892

--- form-scripts/form_scripts.js
+++ form-scripts/form_scripts.js
@@ -1,4 +1,5 @@
-function addFields(){
+function addFields()
+{
     // Number of inputs to create
     var number = document.getElementById("sources").value;
     // Container <div> where dynamic content will be placed
@@ -7,7 +8,14 @@ function addFields(){
     while (container.hasChildNodes()) {
         container.removeChild(container.lastChild);
     }
-    for (i=0;i<number;i++){
+    // read key in case back space was used
+    var key = event.keyCode || event.charCode;
+    if( key== 8 || key == 46 )
+    {
+      return false;
+    }
+    for (i=0;i<number;i++)
+    {
         // Append a node with a random text
         container.appendChild(document.createTextNode("Source " + (i+1) + ": "));
         // Create an <input> element, set its type and name attributes
@@ -18,22 +26,26 @@ function addFields(){
         // Append a line break
         container.appendChild(document.createElement("br"));
     }
-  }
+}
 
-function validateFormOnSubmit(theForm) {
+function validateFormOnSubmit(theForm)
+{
     var reason = "";
     reason += validateSourceMemebers(theForm.name);
 
-    if (reason != "") {
+    if (reason != "")
+    {
         alert("Some fields need correction:\n" + reason);
-    } else {
+    }else
+    {
         var mySettingsObj = {
           "DataRate":document.getElementsByName('DataRate').value,
           "Range":document.getElementsByName('Range').value,
           "Resolution":document.getElementsByName('Resolution').value,
           "Sources":document.getElementsByName('Sources').value,
         }
-        for(i=0;i<document.getElementsByName('Sources').value;i++){
+        for(i=0;i<document.getElementsByName('Sources').value;i++)
+        {
           mySettingsObj.put("Source"+ i, document.getElementsByName("member" + i).value);
         }
         var mySettingsObjString = JSON.stringify(mySettingsObj);
